4913eef6828e0bed59b634108115bba19ab6ec4f
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-01-24 16:58:38 -0300

    se agrego un state al input de tareas

--- src/components/FormularioTareas.jsx
+++ src/components/FormularioTareas.jsx
@@ -1,6 +1,9 @@
 import { Form, Button } from "react-bootstrap";
+import { useState } from "react";
 
 const FormularioTareas = () => {
+const [tarea, setTarea] = useState('');
+
   return (
     <Form>
       <Form.Group className="mb-3 d-flex justify-content-between">
@@ -9,6 +12,8 @@ const FormularioTareas = () => {
           placeholder="Agregar Tarea"
           minLength={3}
           maxLength={50}
+          onChange={(e)=> setTarea(e.target.value)} 
+          value={tarea}
         />
         <Button variant="primary" className="mx-2" type='submit'>
           Agregar
