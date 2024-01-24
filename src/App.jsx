7e06c5e6c6918a50a85de33d4808e83d3aef282d
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-01-23 21:00:46 -0300

    se agrego componenete container de react-bootstrap y el titulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Container>
+        <h1 className="text-center my-4">Lista de Tareas</h1>
+      </Container>
+    </>
+  );
 }
 
 export default App;
