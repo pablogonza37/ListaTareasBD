350ab413774c00bd7e24a9c784251d4cda9d31ca
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-02-25 20:48:12 -0300

    se agregó check a cada tarea creada

--- src/components/ItemTarea.jsx
+++ src/components/ItemTarea.jsx
@@ -1,9 +1,21 @@
-import { Button, ListGroup } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, ListGroup, Form } from "react-bootstrap";
 
 const ItemTarea = ({ nombreTarea, borrarTarea }) => {
+  const [checked, setChecked] = useState(false);
+
+  const handleCheck = () => {
+    setChecked(!checked);
+  };
+
   return (
     <ListGroup.Item className="d-flex justify-content-between">
-      {nombreTarea}{" "}
+      <Form.Check
+        type="checkbox"
+        label={nombreTarea}
+        checked={checked}
+        onChange={handleCheck}
+      />
       <Button variant="danger" onClick={() => borrarTarea(nombreTarea)}>
         Eliminar
       </Button>
@@ -12,3 +24,4 @@ const ItemTarea = ({ nombreTarea, borrarTarea }) => {
 };
 
 export default ItemTarea;
+
