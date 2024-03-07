f8387216fe085d305b771b7996dc567ac0654053
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-03-07 19:54:26 -0300

    se agregó el spinner

--- src/components/FormularioTareas.jsx
+++ src/components/FormularioTareas.jsx
@@ -1,12 +1,17 @@
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Spinner } from "react-bootstrap";
 import ListaTareas from "./ListaTareas";
 import { useState, useEffect } from "react";
-import { agregarTareasAPI, borrarTareaAPI, leerTareasAPI } from "../helpers/queries";
+import {
+  agregarTareasAPI,
+  borrarTareaAPI,
+  leerTareasAPI,
+} from "../helpers/queries";
 import { useForm } from "react-hook-form";
 
 const FormularioTareas = () => {
   const [tareas, setTareas] = useState([]);
-  const [error, setError] = useState(null); 
+  const [error, setError] = useState(null);
+  const [mostrarSpinner, setMostrarSpinner] = useState(true);
   const {
     register,
     handleSubmit,
@@ -20,22 +25,29 @@ const FormularioTareas = () => {
 
   const consultarAPI = async () => {
     try {
+      setMostrarSpinner(true);
       const respuesta = await leerTareasAPI();
       setTareas(respuesta);
-      setError(null); 
+      setError(null);
+      setMostrarSpinner(false);
     } catch (error) {
       console.log(error);
-      setError("Error al cargar las tareas desde la API"); 
+      setError("Error al cargar las tareas desde la API");
+      setMostrarSpinner(false);
     }
   };
 
   const productoValidado = async (tarea) => {
     try {
+      setMostrarSpinner(true);
       const respuesta = await agregarTareasAPI(tarea);
       reset();
-      consultarAPI();    
+      consultarAPI();
+      setError(null);
+      setMostrarSpinner(false);
     } catch (error) {
-      console.log(error); 
+      console.log(error);
+      setMostrarSpinner(false);
     }
   };
 
@@ -48,46 +60,56 @@ const FormularioTareas = () => {
     }
   };
 
+  const mostrarComponente = mostrarSpinner ? (
+    <div className="my-4 text-center">
+      <Spinner animation="border" variant="success" />
+    </div>
+  ) : (
+    <div>
+      {!error && tareas.length === 0 && (
+        <div className="alert alert-info mt-3">No hay tareas.</div>
+      )}
+      {tareas.length > 0 && (
+        <div>
+          <ListaTareas
+            tareas={tareas}
+            borrarTarea={borrarTarea}
+            error={error}
+          ></ListaTareas>
+        </div>
+      )}
+    </div>
+  );
+
   return (
     <section>
-      {error && <div className="alert alert-info mt-3">{error}</div>}
-    
       <Form onSubmit={handleSubmit(productoValidado)}>
         <Form.Group className="d-flex justify-content-between">
           <Form.Control
             id="tareaInput"
             type="text"
-            placeholder="Agregar Tarea"  
+            placeholder="Agregar Tarea"
             {...register("tarea", {
               required: "El campo es obligatorio",
               minLength: {
                 value: 3,
-                message:
-                  "La tarea debe tener como mínimo 3 caracteres",
+                message: "La tarea debe tener como mínimo 3 caracteres",
               },
               maxLength: {
                 value: 50,
-                message:
-                  "La tarea debe tener como máximo 50 caracteres",
+                message: "La tarea debe tener como máximo 50 caracteres",
               },
             })}
           />
-          
+
           <Button variant="success" className="mx-2" type="submit">
             Agregar
           </Button>
         </Form.Group>
-        <Form.Text className="text-warning">
-            {errors.tarea?.message}
-          </Form.Text>
+        <Form.Text className="text-warning">{errors.tarea?.message}</Form.Text>
       </Form>
-      {!error && tareas.length === 0 && (
-        <div className="alert alert-info mt-3">No hay tareas.</div>
-      )}
-       {tareas.length > 0 && ( <div>
-      <ListaTareas tareas={tareas} borrarTarea={borrarTarea} error={error}></ListaTareas>
-      </div>
-      )}
+      {error && <div className="alert alert-info mt-3">{error}</div>}
+      {mostrarComponente}
     </section>
   );
 };
