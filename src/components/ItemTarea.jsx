753813007815eee2c3cb9d7be8c9ce80b47d3da9
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-03-06 23:58:28 -0300

    se crearon los archivos db.json y .env

--- src/components/ItemTarea.jsx
+++ src/components/ItemTarea.jsx
@@ -1,22 +1,10 @@
-import React, { useState } from "react";
-import { Button, ListGroup, Form } from "react-bootstrap";
+import { Button, ListGroup, Form, FormLabel } from "react-bootstrap";
 
 const ItemTarea = ({ nombreTarea, borrarTarea }) => {
-  const [checked, setChecked] = useState(false);
-
-  const handleCheck = () => {
-    setChecked(!checked);
-  };
-
+ 
   return (
     <ListGroup.Item className="d-flex justify-content-between">
-      <Form.Check
-        type="checkbox"
-        label={nombreTarea}
-        checked={checked}
-        onChange={handleCheck}
-        className="overflow-auto"
-      />
+      <FormLabel className="overflow-auto">{nombreTarea}</FormLabel>
       <Button variant="danger" onClick={() => borrarTarea(nombreTarea)}>
       <i className="bi bi-trash"></i>
       </Button>
