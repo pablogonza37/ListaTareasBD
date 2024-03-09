5ce0319bcc6b041e803a04cb9786b4eac57b43e4
Pablo Gonzalez <pab.gonzalez3798@gmail.com>
2024-03-09 17:26:56 -0300

    se agregaron las ventanas de sweetAlert2

--- src/components/FormularioTareas.jsx
+++ src/components/FormularioTareas.jsx
@@ -9,7 +9,7 @@ import {
   obtenerTareaAPI,
 } from "../helpers/queries";
 import { set, useForm } from "react-hook-form";
-import Modal from 'react-bootstrap/Modal';
+import Swal from "sweetalert2";
 
 const FormularioTareas = () => {
   const [tareas, setTareas] = useState([]);
@@ -24,7 +24,7 @@ const FormularioTareas = () => {
   } = useForm();
   const [idTarea, setIdTarea] = useState();
   const [editar, setEditar] = useState(false);
-  const [btnInput, setBtnInput] = useState('Agregar')
+  const [btnInput, setBtnInput] = useState("Agregar");
 
   useEffect(() => {
     consultarAPI();
@@ -45,53 +45,99 @@ const FormularioTareas = () => {
   };
 
   const productoValidado = async (tarea) => {
-    if(editar){  
-     const respuesta = await editarTareaAPI(tarea, idTarea); 
-     console.log(respuesta);
-     const listaTareas = await leerTareasAPI()
-      setTareas(listaTareas);
-      setEditar(false);
-      setBtnInput('Agregar');
-      reset();
-    }else{
-    try {   
-     const respuesta = await agregarTareasAPI(tarea);
-      const listaTareas = await leerTareasAPI()
-      setTareas(listaTareas)
-      setError(null);   
-      reset();
-    } catch (error) {
-      console.log(error);
+    if (editar) {
+      const respuesta = await editarTareaAPI(tarea, idTarea);
+      if (respuesta.status === 200) {
+        Swal.fire({
+          title: "Tarea modificado!",
+          text: `La tarea fue modificada correctamente`,
+          icon: "success",
+        });
+        const listaTareas = await leerTareasAPI();
+        setTareas(listaTareas);
+        setEditar(false);
+        setBtnInput("Agregar");
+        reset();
+      } else {
+        Swal.fire({
+          title: "Ocurrio un error!",
+          text: `La tarea no pudo ser modificada. Intente esta operacion en unos minutos`,
+          icon: "error",
+        });
+      }
+    } else {
+      try {
+        const respuesta = await agregarTareasAPI(tarea);
+        if (respuesta.status === 201) {
+          const listaTareas = await leerTareasAPI();
+          setTareas(listaTareas);
+          setError(null);
+          reset();
+          Swal.fire({
+            title: "Tarea creada!",
+            text: `La tarea fue creada correctamente`,
+            icon: "success",
+          });
+        } else {
+          Swal.fire({
+            title: "Ocurrio un error!",
+            text: `La tarea no pudo ser creada. Intente esta opercion en unos minutos`,
+            icon: "error",
+          });
+        }
+      } catch (error) {
+        console.log(error);
+      }
     }
-  }
   };
 
   const borrarTarea = async (id) => {
-    try {
-      const respuesta = await borrarTareaAPI(id);
-      const listaTareas = await leerTareasAPI()
-      setTareas(listaTareas)
-    } catch (error) {
-      console.log(error);
-    }
+    Swal.fire({
+      title: "¿Estas seguro de eliminar la tarea?",
+      text: "No se puede revertir este proceso",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Borrar",
+      cancelButtonText: "Cancelar",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const respuesta = await borrarTareaAPI(id);
+        if (respuesta.status === 200) {
+          const listaTareas = await leerTareasAPI();
+          setTareas(listaTareas);
+          Swal.fire({
+            title: "Tarea eliminada",
+            text: `La tarea fue eliminada correctamente`,
+            icon: "success",
+          });
+        } else {
+          Swal.fire({
+            title: "Ocurrio un error",
+            text: `La tarea no pudo ser eliminada. Intente realizar esta operación en unos minutos`,
+            icon: "error",
+          });
+        }
+      }
+    });
   };
 
   const cargarDatosTarea = async (id) => {
     setEditar(true);
-    setBtnInput('Guardar');
+    setBtnInput("Guardar");
     try {
       const respuesta = await obtenerTareaAPI(id);
       if (respuesta.status === 200) {
         const tareaEncontrada = await respuesta.json();
         setValue("tarea", tareaEncontrada.tarea);
-        setIdTarea(id)
+        setIdTarea(id);
       }
     } catch (error) {
       console.log(error);
     }
   };
 
- 
   const mostrarComponente = mostrarSpinner ? (
     <div className="my-4 text-center">
       <Spinner animation="border" variant="success" />
